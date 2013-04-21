83c76e7f3e26d5e89a9bd12ad4be39787d59cc08
Steve Krouse <skrouse@seas.upenn.edu>
2013-04-21 03:32:00 -0400

    shit

--- app.py
+++ app.py
@@ -67,5 +67,5 @@ def get_data(_id):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    p = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=p)
