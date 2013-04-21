3af653f81c6d438152e235208c3b00142bc83f75
Steve Krouse <skrouse@seas.upenn.edu>
2013-04-21 01:31:11 -0400

    basic functionality working

--- ToTheBeat.py
+++ ToTheBeat.py
@@ -4,7 +4,8 @@ from json import dumps
 from pymongo import MongoClient
 from bson import ObjectId, json_util
 from flask import request
-from flask import redirect, url_for, jsonify
+from flask import redirect, url_for
+import json
 
 app = Flask(__name__)
 
@@ -13,37 +14,41 @@ db = client.to_the_beat
 collection = db.slideshow_collection
 
 
-@app.route('/<_id>')
 @app.route('/')
-def home(_id=None):
-    return render_template('index.html', _id=_id)
+def home():
+    return render_template('index.html')
 
 
-@app.route('/create', methods=['POST', 'GET'])
+@app.route('/create', methods=['GET', 'POST'])
 def create():
-    if request.method == 'POST':
-        return redirect(url_for('home'))
+    if request.method == 'GET':
+        images = [str(url) for url in request.args.get('images').split(',')]
+        song = "'" + str(request.args.get('song')) + "'"
+        return render_template('create.html', images=images, song=song)
     else:
-        vid = jsonifyRequest(request)
-        _id = collection.insert(vid)
-        return redirect(url_for('home', _id=_id))
+        form = request.form
+        images = form.getlist('images[]')
+        song = form.getlist('song')[0]
+        intervals = form.getlist('intervals[]')
+        _id = collection.insert({'images': images,
+                                 'song': song,
+                                 'intervals': intervals})
+        return '/watch/' + str(_id)
+
+
+@app.route('/watch/<_id>')
+def watch(_id):
+    data = collection.find_one({"_id": ObjectId(_id)})
+    return render_template('watching.html',
+                           images=[str(s) for s in data['images']],
+                           song="'" + str(data['song']) + "'",
+                           intervals=[float(i) for i in data['intervals']])
 
 
 @app.route('/get_data/<_id>')
 def get_data(_id):
     data = collection.find_one({"_id": ObjectId(_id)})
     return dumps(data, sort_keys=True, indent=4, default=json_util.default)
-
-
-def jsonifyRequest(request):
-    images = ['https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR3doxGuyD7TzHL6R8b35G3_UtpYmRjzGliUX7YfnMkGqrW9Sz0',
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbZ4LWZzZzJxm3psPlfU2LhULTuiofAz2huEqihTpkmHEIB1HH',
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-6QRMoY1p0svcMARjR4dUSymYQsFzY3Wy7SpVbbPI9AEql5DYhA',
-              'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQMFZVteoNJxkEp-DBlQm74mjfDjUQOEubR3eFA3Hs0D3TA3oB97Q',
-              'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSUw0YMb0Cz9yHUKM5cleRCnAVEZ6FpAc81eS795kSZAyczfoJr']
-    song = "hi"
-    intervals = [5000, 2500, 1000, 100, 10]
-    return {'images': images, 'song': song, 'intervals': intervals}
 
 
 if __name__ == "__main__":
